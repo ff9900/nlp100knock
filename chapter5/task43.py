9c8b75a3c0d5daa8eb4e713c98bc878261e42bc0
ff9900 <d30822tt@gmail.com>
2016-08-19 15:28:08 +0900

    Add files via upload

--- chapter5/task43.py
+++ chapter5/task43.py
@@ -7,15 +7,13 @@ chunks_list = task41()
 
 
 for chunks in chunks_list:
-    for index in range(len(chunks)):
-        try:
-            pre = '名詞' in [morph.pos for morph in chunks[index].morphs]
-            suf = '動詞' in [morph.pos for morph in chunks[index+1].morphs]
+    for chunk in chunks:
+        if chunk.dst != '-1':
+            pre = '名詞' in [morph.pos for morph in chunk.morphs]
+            suf = '動詞' in [morph.pos for morph in chunks[int(chunk.dst)].morphs]
             if pre and suf:
                 phrase = ''
-                phrase += ''.join([morph.surface for morph in chunks[index].morphs if morph.pos != '記号'])
+                phrase += ''.join([morph.surface for morph in chunk.morphs if morph.pos != '記号'])
                 phrase += '\t'
-                phrase += ''.join([morph.surface for morph in chunks[index+1].morphs if morph.pos != '記号'])
+                phrase += ''.join([morph.surface for morph in chunks[int(chunk.dst)].morphs if morph.pos != '記号'])
                 print phrase
-        except:
-            continue
